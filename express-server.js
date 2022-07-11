6681b54a6a897a3b4e9fdae2e816d4890a3ec9a6
Rafalesson <rafaalesson35@gmail.com>
2022-07-11 11:42:39 -0300

    primeiros passos no express

---
+++ express-server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    console.log(req.headers)
+    res.send('Hello from server.')
+})
+
+app.listen(4000)
+console.log('server listen on http://localhost:4000')
